386e1215180939de2044eae090a86046a4e811ec
ana07012007 <ananya.7jan@gmail.com>
2026-01-21 14:16:49 +0530

    Time: 3 ms (13.53%) | Memory: 43.6 MB (31.54%) - LeetSync

---
+++ 75-sort-colors/sort-colors.java
@@ -0,0 +1,12 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        // for(int i =0; i< nums.length ; i++){
+        //     for(int j = i ; j< nums.length ; j++){
+
+        //     }
+        // }
+        
+        Arrays.sort(nums);
+        
+    }
+}
