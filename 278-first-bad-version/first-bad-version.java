cfe8c3cd6857934694ddb936c2da2216deea92dc
ana07012007 <ananya.7jan@gmail.com>
2026-02-04 14:26:30 +0530

    Time: 14 ms (52.38%) | Memory: 42 MB (83.80%) - LeetSync

--- 278-first-bad-version/first-bad-version.java
+++ 278-first-bad-version/first-bad-version.java
@@ -4,7 +4,7 @@
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
         int start =1;
-        int end = n;
+        int end = n-1;
 
         while(start<= end){
             int mid = start + (end-start)/2;
