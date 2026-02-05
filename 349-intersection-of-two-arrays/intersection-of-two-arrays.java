3a9fc00041b526d48b47b232555c2c924ff69bb3
ana07012007 <ananya.7jan@gmail.com>
2026-02-05 19:14:45 +0530

    Time: 7 ms (18.10%) | Memory: 45.1 MB (52.55%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,28 +1,58 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
         ArrayList<Integer> list = new ArrayList<>();
-        
-        for(int i =0; i< nums1.length ; i++){
-            int count = 0;
-            for(int j = 0 ; j< nums2.length ; j++){
-                if(nums1[i] == nums2[j]){
-                    nums2[j]=Integer.MIN_VALUE;
-                    count++;
-                }
+        list = removeduplicate(nums1);
 
-                if(count==1){
-                    list.add(nums1[i]);
-                    count++;
-                }
+        ArrayList<Integer> anslist = new ArrayList<>();
+        Arrays.sort(nums2);
+
+        for(int i =0; i< list.size() ;i++ ){
+            int num = binarysearch(nums2, list.get(i));
+            if(num!=-1){
+                anslist.add(num);
             }
         }
-
-        int[] ans  = new int[list.size()];
-
-        for(int i =0 ; i< list.size() ; i++){
-            ans[i]= list.get(i);
+        int[] ans = new int[anslist.size()];
+        for(int i =0; i<anslist.size() ; i++){
+            ans[i] = anslist.get(i);
         }
 
         return ans;
+        
     }
+
+    static ArrayList<Integer> removeduplicate(int[] arr){
+        Arrays.sort(arr);
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = 0 ; i< arr.length-1 ; i++ ){
+            if(arr[i] != arr[i+1]){
+                list.add(arr[i]);
+            }
+        }
+        list.add(arr[arr.length-1]);
+
+        return list;
+    }
+
+    static int binarysearch(int[] arr , int target){
+        int start =0;
+        int end = arr.length-1;
+
+        while(start<=end){
+            int mid = start + (end-start)/2;
+
+            if(arr[mid]<target){
+                start = mid+1;
+            }else if(arr[mid] > target){
+                end = mid -1;
+            }else{
+                return arr[mid];
+            }
+        }
+
+        return -1;
+    }
+
+    
+    
 }
