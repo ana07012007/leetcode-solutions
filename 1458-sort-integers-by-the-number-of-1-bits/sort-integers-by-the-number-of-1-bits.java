3e5f79c7897ade62b358cc1d603d10331089a1c1
Ananya <ananya.7jan@gmail.com>
2026-04-22 15:50:14 +0530

    Time: 7 ms (89.71%) | Memory: 47.2 MB (10.26%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.java
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.java
@@ -1,23 +1,13 @@
 class Solution {
     public int[] sortByBits(int[] arr) {
-        for(int i = 0 ; i < arr.length ; i++){
-            for(int j = 0 ; j < arr.length - i -1; j++){
-                if(Integer.bitCount(arr[j]) > Integer.bitCount(arr[j+1])){
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
-                }
-                else if(Integer.bitCount(arr[j]) == Integer.bitCount(arr[j+1])){
-                    if(arr[j] > arr[j+1]){
-                        int temp = arr[j];
-                        arr[j] = arr[j+1];
-                        arr[j+1] = temp;
-                    }
-                }
-            }
+        final int MAGIC_NUMBER = 10001;
+        for(int i = 0 ;i < arr. length ; i++){
+            arr[i] += Integer.bitCount(arr[i]) * MAGIC_NUMBER;
         }
-
+        Arrays.sort(arr);
+        for(int i = 0 ; i < arr.length ; i++){
+            arr[i] %= MAGIC_NUMBER;
+        }
         return arr;
     }
-    
 }
