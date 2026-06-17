f0f16c40dd92b59f1ca81f90836c8f8d65764b86
Ananya <ananya.7jan@gmail.com>
2026-06-18 01:35:30 +0530

    Time: 2 ms (98.13%) | Memory: 45 MB (41.85%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,58 +1,22 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        ArrayList<Integer> list = new ArrayList<>();
-        list = removeduplicate(nums1);
+        HashSet<Integer> set1 = new HashSet<>();
+        HashSet<Integer> ans = new HashSet<>();
 
-        ArrayList<Integer> anslist = new ArrayList<>();
-        Arrays.sort(nums2);
-
-        for(int i =0; i< list.size() ;i++ ){
-            int num = binarysearch(nums2, list.get(i));
-            if(num!=-1){
-                anslist.add(num);
+        for(int num : nums1){
+            set1.add(num);
+        }
+        for(int num : nums2){
+            if(set1.contains(num)){
+                ans.add(num);
             }
         }
-        int[] ans = new int[anslist.size()];
-        for(int i =0; i<anslist.size() ; i++){
-            ans[i] = anslist.get(i);
+        int[] arr = new int[ans.size()];
+        int i = 0;
+        for(int num : ans){
+            arr[i++] = num;
         }
 
-        return ans;
-        
+        return arr;
     }
-
-    static ArrayList<Integer> removeduplicate(int[] arr){
-        Arrays.sort(arr);
-        ArrayList<Integer> list = new ArrayList<>();
-        for(int i = 0 ; i< arr.length-1 ; i++ ){
-            if(arr[i] != arr[i+1]){
-                list.add(arr[i]);
-            }
-        }
-        list.add(arr[arr.length-1]);
-
-        return list;
-    }
-
-    static int binarysearch(int[] arr , int target){
-        int start =0;
-        int end = arr.length-1;
-
-        while(start<=end){
-            int mid = start + (end-start)/2;
-
-            if(arr[mid]<target){
-                start = mid+1;
-            }else if(arr[mid] > target){
-                end = mid -1;
-            }else{
-                return arr[mid];
-            }
-        }
-
-        return -1;
-    }
-
-    
-    
 }
