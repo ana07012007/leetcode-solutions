cf73aa1c782177528ee9347acaa24effecb3edaf
Ananya <ananya.7jan@gmail.com>
2026-06-21 01:15:00 +0530

    Time: 2 ms (73.62%) | Memory: 43 MB (70.34%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.java
+++ 409-longest-palindrome/longest-palindrome.java
@@ -1,22 +1,20 @@
 class Solution {
     public int longestPalindrome(String s) {
-       HashMap<Character , Integer> map = new HashMap<>();
-
-       for(char c : s.toCharArray()){
-        map.put(c , map.getOrDefault(c , 0)+1);
-       } 
-
-       boolean taken = false;
-       int ans = 0;
-       
-       for(Integer value : map.values()){
-        ans += value - (value %2);
-        if(value % 2 != 0 && !taken){
-            ans += 1;
-            taken = true;
+        int ans = 0;
+        int[] freq = new int[58];
+        for(char c : s.toCharArray()){
+            freq[c - 'A']++;
+            if(freq[c - 'A']% 2 == 0){
+                ans+=2;
+            }
         }
-       }
+        for(int num : freq){
+            if(num % 2 == 1){
+                ans++;
+                break;
+            }
+        }
 
-       return ans;
+        return ans;
     }
 }
