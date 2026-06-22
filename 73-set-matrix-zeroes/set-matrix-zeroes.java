6cb74be2716be30f8c962c66936e40971c59fcdb
Ananya <ananya.7jan@gmail.com>
2026-06-23 00:54:36 +0530

    Time: 1 ms (98.46%) | Memory: 47.8 MB (12.26%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,43 +1,48 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
+        int temp = 0;
         for(int i = 0 ; i < matrix.length ; i++){
-            for(int j = 0 ; j < matrix[0].length ; j++){
+            for(int j = 0; j < matrix[0].length ; j++){
                 if(matrix[i][j] == 0){
-                    int top = i;
-                    while(top >= 0){
-                        if(matrix[top][j] != 0){
-                            matrix[top][j] = -10;
-                        }
-                        top--;
+                    if(j == 0){
+                        temp = -10;
+                        matrix[i][0] = -10;
+                    }else{
+                        matrix[0][j] = -10;
+                        matrix[i][0] = -10;
                     }
-                    int bottom = i;
-
-                    while(bottom < matrix.length){
-                        if(matrix[bottom][j] != 0)matrix[bottom][j] = -10;
-                        bottom++;
-                    }
-
-                    int left = j;
-
-                    while(left >= 0){
-                        if(matrix[i][left] != 0)matrix[i][left] = -10;
-                        left--;
-                    }
-
-                    int right = j;
-                    while(right < matrix[0].length){
-                        if(matrix[i][right] != 0)matrix[i][right] = -10;
-                        right++;
-                    }
-
                 }
             }
         }
 
-        for(int i = 0 ; i < matrix.length ; i++){
-            for(int j = 0 ; j < matrix[0].length ; j++){
-                if(matrix[i][j] == -10)matrix[i][j] = 0;
+        for(int i = 1; i < matrix.length ; i++){
+            for(int j = 1 ; j < matrix[0].length ; j++){
+                if(matrix[0][j] == -10 || matrix[i][0] == -10)matrix[i][j] = 0;
             }
         }
+
+        if(matrix[0][0] == -10){
+            for(int j = 0 ; j < matrix[0].length ; j++){
+                matrix[0][j] = 0;
+            }
+        }else{
+            for(int j = 1 ; j < matrix[0].length ; j++){
+                if(matrix[0][j] == -10)matrix[0][j] = 0;
+            }
+        }
+
+        if(temp == -10){
+            for(int i = 0 ; i < matrix.length ; i++){
+                matrix[i][0] = 0;
+            }
+        }
+        else{
+            for(int i = 0 ; i < matrix.length ; i++){
+                if(matrix[i][0] == -10){
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+
     }
 }
