9c5e7cb8c465dc897ad3740936ddd8da31eb0858
Ananya <ananya.7jan@gmail.com>
2026-06-29 22:42:54 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (91.70%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.java
+++ 119-pascals-triangle-ii/pascals-triangle-ii.java
@@ -2,22 +2,13 @@ class Solution {
     public List<Integer> getRow(int rowIndex) {
         
         List<Integer> list = new ArrayList<>();
-
-        for(int i = 0 ;i <= rowIndex ; i++){
-            int num =(int) com(rowIndex , i);
-            list.add(num);
-        }
-
-        return list;
-    }
-
-    static long com(int row , int col){
         long ans = 1;
-        for(int i = 0 ; i < col ; i++){
-            ans *= row - i;
+        for(int i = 0 ;i <= rowIndex ; i++){
+            list.add((int)ans);
+            ans *= (rowIndex - i);
             ans /= i+1;
         }
 
-        return ans;
+        return list;
     }
 }
