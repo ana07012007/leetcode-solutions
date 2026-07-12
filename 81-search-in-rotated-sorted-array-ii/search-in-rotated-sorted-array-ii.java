fa7b90de2e5819d19afd4c714f3c5150e72b8809
Ananya <ananya.7jan@gmail.com>
2026-07-13 03:08:01 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (65.23%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,67 +1,53 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        int k = pivot(nums);
+        int pivot = pivot(nums);
+        boolean ans = binarysearch(nums , 0 , pivot , target);
 
-        int start = 0;
-        int end = k;
-
-        if(binarysearch(nums , target , start , end)){
-            return true;
-        }else{
-            start = k+1;
-            end = nums.length-1;
-            if(binarysearch(nums , target , start , end)){
-                return true;
-            }
+        if(!ans){
+            ans = binarysearch(nums , pivot+1 , nums.length - 1, target);
         }
 
-        return false;
+        return ans;
     }
 
-    static int pivot(int[] nums){
-        int start =0;
-        int end = nums.length-1;
-
-        while(start<end){
-            int mid = start + (end-start)/2;
-
-            if(nums[mid] > nums[mid+1]){
+    static int pivot(int[] arr){
+        int start = 0;
+        int end = arr.length -1;
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            if(mid + 1 < arr.length && arr[mid] > arr[mid] + 1){
                 return mid;
             }
-            if(nums[mid] < nums[end]){
-                end = mid;
-            
-            }else if(nums[mid] > nums[start]){
+            if(arr[mid] > arr[start]){
                 start = mid;
-            }else if(nums[start]==nums[start+1]){
+            }
+            else if(arr[mid] < arr[end]){
+                end = mid;
+            }else if(start+1 < arr.length && arr[start] == arr[start+1]){
                 start++;
             }
             else{
                 end--;
             }
         }
-            
-        
-
         return start;
     }
 
-
-    static boolean binarysearch(int[] arr , int target , int start , int end){
-
-        while(start<=end){
-            int mid = start + (end-start)/3;
+    static boolean binarysearch(int[] arr , int start , int end , int target){
+        while(start <= end){
+            int mid = start + (end - start)/2;
 
             if(arr[mid] < target){
-                start = mid+1;
-            }
-            else if(arr[mid] > target){
-                end = mid -1;
+                start = mid + 1;
+            }else if(arr[mid] > target){
+                end = mid - 1;
             }
             else{
                 return true;
             }
         }
+
         return false;
     }
+
 }
