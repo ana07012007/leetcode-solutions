85c0b28dbf12a867196ada73f1ddee9e398c4f92
Ananya <ananya.7jan@gmail.com>
2026-08-04 21:31:54 +0530

    Time: 1 ms (100.00%) | Memory: 49 MB (33.26%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,22 +1,33 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int[] temp = new int[nums1.length + nums2.length];
-        int i1 = 0;
-        int i2 = 0;
-        int k = 0;
-        while(i1 < nums1.length && i2 < nums2.length){
-            if(nums1[i1] <= nums2[i2])temp[k++] = nums1[i1++];
-            else temp[k++] = nums2[i2++];
+        
+        if(nums1.length > nums2.length)return findMedianSortedArrays(nums2 , nums1);
+        int start = 0;
+        int end = nums1.length;
+        int k = (nums1.length + nums2.length + 1)/2;
+        while(start <= end){
+            int mid1 = start + (end - start)/2;
+            int mid2 = k - mid1;
+            int l1 = Integer.MIN_VALUE;
+            if(mid1 - 1 >= 0)l1 = nums1[mid1 - 1];
+            int l2 = Integer.MIN_VALUE;
+            if(mid2 - 1 >= 0)l2 = nums2[mid2 - 1];
+            int r1 = Integer.MAX_VALUE;
+            if(mid1 < nums1.length)r1 = nums1[mid1];
+            int r2 = Integer.MAX_VALUE;
+            if(mid2 < nums2.length)r2 = nums2[mid2];
+            if(l1 <= r2 && l2 <= r1){
+                if((nums1.length + nums2.length) % 2 == 1)return Math.max(l1 , l2);
+                return (double)(Math.max(l1 , l2) + Math.min(r1 , r2))/2.0;
+            }else if(l1 > r2){
+                end = mid1 - 1;
+            }
+            else{
+                start = mid1 + 1;
+            }
+            
         }
-        while(i1 < nums1.length)temp[k++] = nums1[i1++];
-        while(i2 < nums2.length)temp[k++] = nums2[i2++];
 
-        if(temp.length % 2 == 1){
-            return (double) temp[temp.length/2];
-        }
-        else{
-            double ans = (double) (temp[temp.length /2] + temp[(temp.length - 1)/2])/2;
-            return ans;
-        }
+        return 0;
     }
 }
