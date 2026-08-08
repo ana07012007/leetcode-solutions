fc3f64fc7c997ca11aedbadee11027cd4ca33279
Ananya <ananya.7jan@gmail.com>
2026-08-08 20:16:20 +0530

    Time: 1 ms (80.66%) | Memory: 43.6 MB (25.96%) - LeetSync

---
+++ 812-rotate-string/rotate-string.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if(s.length() != goal.length())return false;
+
+        for(int i = 0; i < s.length() ; i++){
+            StringBuilder sb = new StringBuilder();
+
+            for(int j = i; j < s.length(); j++){
+                sb.append(s.charAt(j));
+            }
+            for(int j = 0 ; j < i ; j++){
+                sb.append(s.charAt(j));
+            }
+
+            if(sb.toString().equals(goal))return true;
+        }
+
+        return false;
+    }
+}
