495629618b48ace5d9cc73a22dc1e9f2b25746a8
Ananya <ananya.7jan@gmail.com>
2026-09-02 13:03:34 +0530

    Time: 1 ms (17.15%) | Memory: 43.6 MB (27.12%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,23 +1,19 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] grid) {
         if(grid[0][0] == 1)return 0;
-        int[][] ans = new int[grid.length][grid[0].length];
-        ans[0][0] = 1;
-        for(int i = 0 ; i < grid.length ; i++){
-            for(int j = 0 ; j < grid[i].length ; j++){
-                if(grid[i][j] == 1){
-                    ans[i][j] = 0;
-                }
-                else if(i - 1 >= 0 && j - 1 < 0){
-                    ans[i][j] = ans[i- 1][j];
-                }else if(j - 1 >= 0 && i - 1 < 0){
-                    ans[i][j] = ans[i][j -1];
-                }else if(i - 1 >= 0 && j - 1 >= 0){
-                    ans[i][j] = ans[i -1][j] + ans[i][j -1];
-                }
+        grid[0][0] = 1;
+        int m = grid.length;
+        int n = grid[0].length;
+
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(i == 0 && j == 0)continue;
+                if(grid[i][j] == 1)grid[i][j] = 0;
+                else if(i - 1 < 0 && j - 1 >= 0)grid[i][j] = grid[i][j-1];
+                else if(i - 1 >= 0 && j - 1 < 0)grid[i][j] = grid[i - 1][j];
+                else if(i - 1 >= 0 && j - 1 >= 0)grid[i][j] = grid[i - 1][j] + grid[i][j -1];
             }
         }
-
-        return ans[grid.length -1 ][grid[0].length -1];
+        return grid[m -1][n -1];
     }
 }
