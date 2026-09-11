78f2847089fe3200d0be935fc39d2a529fabe57d
Ananya <ananya.7jan@gmail.com>
2026-09-11 22:51:09 +0530

    Time: 8 ms (14.22%) | Memory: 45.6 MB (19.13%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -2,19 +2,16 @@ class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> temp = new ArrayList<>();
-        HashSet<List<Integer>> set = new HashSet<>();
+    
         Arrays.sort(candidates);
 
-        return solve(candidates , target , ans , temp , 0 , 0 , set);
+        return solve(candidates , target , ans , temp , 0 , 0 );
 
     }
 
-    public List<List<Integer>> solve(int[] arr , int target , List<List<Integer>> ans , List<Integer> curr , int sum , int index , HashSet<List<Integer>> set){
+    public List<List<Integer>> solve(int[] arr , int target , List<List<Integer>> ans , List<Integer> curr , int sum , int index){
         if(sum == target){
-            if(!set.contains(curr)){
-                set.add(new ArrayList<>(curr));
-                ans.add(new ArrayList<>(curr));
-            }
+            ans.add(new ArrayList<>(curr));
             return ans;
         }
         else if(sum > target){
@@ -24,7 +21,7 @@ class Solution {
         for(int i = index ; i < arr.length ;i++){
             if(i > index && arr[i] == arr[i-1])continue;
             curr.add(arr[i]);
-            ans = solve(arr , target , ans , curr , sum + arr[i] , i + 1 , set);
+            ans = solve(arr , target , ans , curr , sum + arr[i] , i + 1);
             curr.remove(curr.size() - 1);
         }
         return ans;
