85aa4032d9e420b7c58e8da2691e935182ad9199
Ananya <ananya.7jan@gmail.com>
2026-09-13 20:11:07 +0530

    Time: 79 ms (96.66%) | Memory: 43.1 MB (28.42%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,44 +1,49 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        HashSet<String> set = new HashSet<>();
-
-        for(int i = 0 ; i < board.length ; i++){
+       for(int i = 0 ; i < board.length ; i++){
             for(int j = 0 ; j < board[0].length ; j++){
-                if(board[i][j] == word.charAt(0)){set.add(i + "," + j);
-                if(solve(board , word , set , i , j , 1 ))return true;
-                set.remove(i + "," + j);}
+                if(board[i][j] == word.charAt(0)){
+                    char temp = board[i][j];
+                    board[i][j] = '.';
+                    if(solve(board , word , i , j , 1 ))return true;
+                    board[i][j] = temp;
+                }
             }
         }
         return false;
 
     }
 
-    public boolean solve(char[][] mat , String s ,HashSet<String> set , int i , int j , int index ){
+    public boolean solve(char[][] mat , String s, int i , int j , int index ){
         if(index == s.length()){
             return true;
         }
        
 
-        if(i - 1 >= 0 && !set.contains((i - 1) + "," + j) && s.charAt(index) == mat[i - 1][j]){
+        if(i - 1 >= 0 && s.charAt(index) == mat[i - 1][j]){
 
-            set.add((i - 1) + "," + j);
-            if(solve(mat , s , set , i-1 , j , index + 1)) return true;
-            set.remove((i - 1) + "," + j);
+            char temp = mat[i - 1][j];
+            mat[i - 1][j] = '.';
+            if(solve(mat , s , i-1 , j , index + 1)) return true;
+            mat[i - 1][j] = temp;        
         }
-        if(i + 1 < mat.length && !set.contains((i + 1) + "," + j) && s.charAt(index) == mat[i+ 1][j]){
-            set.add((i + 1) + "," + j);
-            if(solve(mat , s , set , i+ 1 , j , index + 1))return true;
-            set.remove((i + 1) + "," + j);
+        if(i + 1 < mat.length && s.charAt(index) == mat[i+ 1][j]){
+            char temp = mat[i + 1][j];
+            mat[i + 1][j] = '.';
+            if(solve(mat , s , i+ 1 , j , index + 1))return true;
+            mat[i + 1][j] = temp;        
         }
-        if(j - 1 >= 0 && !set.contains(i + "," + (j - 1)) && s.charAt(index) == mat[i][j -1]){
-            set.add(i + "," + (j - 1));
-            if(solve(mat , s , set , i , j -1 , index + 1)) return true;
-            set.remove(i + "," + (j - 1));
+        if(j - 1 >= 0 && s.charAt(index) == mat[i][j -1]){
+            char temp = mat[i][j - 1];
+            mat[i][j - 1] = '.';
+            if(solve(mat , s , i , j -1 , index + 1)) return true;
+            mat[i][j - 1] = temp;
         }
-        if(j + 1 < mat[0].length && !set.contains(i + "," + (j + 1)) && s.charAt(index) == mat[i][j + 1]){
-            set.add(i + "," + (j + 1));
-            if(solve(mat , s , set , i , j + 1, index + 1)) return true;
-            set.remove(i + "," + (j + 1));
+        if(j + 1 < mat[0].length && s.charAt(index) == mat[i][j + 1]){
+            char temp = mat[i][j + 1];
+            mat[i][j + 1] = '.';
+            if(solve(mat , s , i , j + 1, index + 1)) return true;
+            mat[i][j + 1] = temp;
         }
 
         return false;
